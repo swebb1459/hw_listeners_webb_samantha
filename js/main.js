f9afd44b72fed157ce5106ce820c514a1e4fd80e
Samantha Webb <samanthawebb@outlook.com>
2016-03-18 23:32:50 -0400

    add function to input values to console.log

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
 var submitButton = document.getElementById( 'button' );
 
 function logSubmit() {
-    alert( 'hey' );
+    var firstName = document.getElementById( 'firstName' );
+    var lastName = document.getElementById( 'lastName' );
+    var email = document.getElementById( 'email' );
+    var message = document.getElementById( 'message' );
+
+    console.log( 'The first name is: ' + firstName.value );
+    console.log( 'The last name is: ' + lastName.value );
+    console.log( 'The email is: ' + email.value );
+    console.log( 'The message is: ' + message.value );
 }
 
 submitButton.addEventListener( 'click', logSubmit );
