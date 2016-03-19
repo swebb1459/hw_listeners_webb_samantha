b0c5e9a4936fba431f8b77b96f77c1ed8d17f262
Samantha Webb <samanthawebb@outlook.com>
2016-03-19 18:16:48 -0400

    add code so paragraph prints once and changes click count after

--- js/main.js
+++ js/main.js
@@ -1,14 +1,21 @@
 var heading = document.getElementById( 'heading1' );
 var i = 0;
 
+heading.addEventListener( 'click', clicking );
+
 function clicking() {
     var paragraph = document.createElement( 'p' );
 
     paragraph.innerHTML = 'This is click number <a id="count">0</a>';
-    document.body.appendChild( paragraph );
 
-    document.getElementById( 'count' ).innerHTML = ++i;
+    if ( i === 0 ) {
+
+        document.body.appendChild( paragraph );
+
+        document.getElementById( 'count' ).innerHTML = ++i;
+
+    } else {
+        document.getElementById( 'count' ).innerHTML = ++i;
+    }
 
 }
-
-heading.addEventListener( 'click', clicking );
