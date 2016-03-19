6ad7ab4ece0d2e326d3e6971b27c747427af501f
Samantha Webb <samanthawebb@outlook.com>
2016-03-18 21:43:19 -0400

    add function clicking with basic element insertion to index.html

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-h1.addEventListener( 'click', clicking );
+var heading = document.getElementById( 'heading1' );
+
+function clicking () {
+    var paragraph = document.createElement( 'div' );
+    paragraph.innerHTML = 'This is click number x';
+    document.body.appendChild( paragraph );
+}
+
+heading.addEventListener( 'click', clicking );
