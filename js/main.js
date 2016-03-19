aa82860584d2ca9c0cd1362fc560543731285c69
Samantha Webb <samanthawebb@outlook.com>
2016-03-19 19:51:26 -0400

    set up event listener for copyImage javascript

---
+++ js/main.js
@@ -0,0 +1,5 @@
+// The purpose of this javascript is to copy an element
+//This event listens for a click
+//When click it copies the image and puts it next to it.
+
+img.addEventListener( 'click', copyImage );
