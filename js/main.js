206e70f5d9d35a3debf724ac328ae9cb476aa5f0
Samantha Webb <samanthawebb@outlook.com>
2016-03-18 21:50:26 -0400

    correct and change createElement to p in clicking function

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 var heading = document.getElementById( 'heading1' );
 
-function clicking () {
-    var paragraph = document.createElement( 'div' );
+function clicking() {
+    var paragraph = document.createElement( 'p' );
+
     paragraph.innerHTML = 'This is click number x';
     document.body.appendChild( paragraph );
 }
