5672dc32477735db914749c12983330a3a15ef46
Samantha Webb <samanthawebb@outlook.com>
2016-03-19 17:25:22 -0400

    add code so page will count number of clicks and insert into html

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
 var heading = document.getElementById( 'heading1' );
+var i = 0;
 
 function clicking() {
     var paragraph = document.createElement( 'p' );
 
-    paragraph.innerHTML = 'This is click number ' + number;
+    paragraph.innerHTML = 'This is click number <a id="count">0</a>';
     document.body.appendChild( paragraph );
+
+    document.getElementById( 'count' ).innerHTML = ++i;
+
 }
 
 heading.addEventListener( 'click', clicking );
